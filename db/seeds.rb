062575e2863d84a5501da374ae7ebe2100bd128f
Michael Mayorov <mmajorov@gmail.com>
2016-02-11 16:44:40 +0300

    products page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts 'Creating products'
+[
+    { name: 'Samsung UE22H5610',            price: 18_990,  image: 'http://mdata.yandex.net/i?path=b0613150311_img_id5308957719092040926.jpeg'},
+    { name: 'Sony Xperia Z3 Compact',       price: 27_490,  image: 'http://mdata.yandex.net/i?path=b0904224335_img_id425230623294204433.jpeg'},
+    { name: 'Lenovo G700',                  price: 24_900,  image: 'http://mdata.yandex.net/i?path=b0819164800_img_id5103345589481248070.jpg'},
+    { name: 'Amazon Kindle 5',              price: 8_150,   image: 'http://mdata.yandex.net/i?path=b1011104710_img_id5245341255920608103.jpg'},
+    { name: 'Canon EOS 600D Kit',           price: 27_840,  image: 'http://mdata.yandex.net/i?path=b0213210535_img_id6351211769282151944.jpg'},
+    { name: 'Apple iPod shuffle 4 2Gb',     price: 3_560,   image: 'http://mdata.yandex.net/i?path=b0129084249_img_id6984693941852072862.jpeg'},
+    { name: 'Samsung Gear 2 Neo',           price: 8_490,   image: 'http://mdata.yandex.net/i?path=b0423085224_img_id5227319824167798490.jpeg'},
+    { name: 'Lenovo IdeaTab A7600 16Gb 3G', price: 16_020,  image: 'http://mdata.yandex.net/i?path=b0521004031_img_id6569655011894724867.jpeg'},
+    { name: 'LG GA-B409 SLCA',              price: 39_230,  image: 'http://mdata.yandex.net/i?path=b0210151125_img_id4639031377487357309.jpg'},
+].each {|p| Product.create p}
