d09fe0e68b90c26dcb0d42c841c59f2191026ff8
Michael Mayorov <mmajorov@gmail.com>
2016-02-11 16:35:58 +0300

    product model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210150932) do
+ActiveRecord::Schema.define(version: 20160211133308) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.integer  "price",       limit: 4
+    t.string   "image",       limit: 255
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
