ca78808574c62bfa3a240be226b7fb8072026bc8
Michael Mayorov <mmajorov@gmail.com>
2016-04-11 10:43:38 +0300

    add phone_number to user migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211210630) do
+ActiveRecord::Schema.define(version: 20160411074232) do
 
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id",     limit: 4
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20160211210630) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "phone_number",           limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
