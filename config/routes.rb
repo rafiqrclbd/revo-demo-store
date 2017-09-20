2efd2d359c5605988484ab036090cce5bbea26fe
pseudoclaws <1297@mail.ru>
2017-09-20 19:48:31 +0500

    [RTECH-756] routes formatted a little

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   root to: 'products#index'
+
   devise_for :users, :controllers => {registrations: :registrations}
+
   resources :products, only: [:index]
+
   resource :cart, only: :show do
     post 'add/:product_id', to: 'carts#add', as: :add_to
     post 'remove/:product_id', to: 'carts#remove', as: :remove_from
   end
+
   resources :orders, only: [:index, :create, :show] do
     resource :payu, controller: :payu, only: [:show]
   end
@@ -13,7 +17,7 @@ Rails.application.routes.draw do
   resources :revo, only: [:show] do
     get :limit, to: 'revo#limit', on: :collection
     post :callback, to: 'revo#callback', on: :collection
-    end
+  end
 
   resources :fullrevo, only: [:show] do
     post :callback, to: 'revo#callback', on: :collection
