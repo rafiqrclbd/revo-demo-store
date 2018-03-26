a5d8455249c6fb9e024f4905f4bd06b4e4e388d2
Albert <aface13@gmail.com>
2018-03-26 19:03:36 +0500

    [RTECH-1620] Add sale price to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106093548) do
+ActiveRecord::Schema.define(version: 20180326140144) do
 
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id",     limit: 4
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20171106093548) do
     t.text     "description", limit: 65535
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.integer  "old_price",   limit: 4
+    t.integer  "sale_price",  limit: 4
   end
 
   create_table "users", force: :cascade do |t|
